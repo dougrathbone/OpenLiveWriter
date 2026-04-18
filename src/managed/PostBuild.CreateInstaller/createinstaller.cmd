05e3181ab9e922c0f70fb6286436eea0c4771b5f
Doug Rathbone <dougrathbone@gmail.com>
2026-04-19 09:11:47 +1000

    chore: migrate installer from Squirrel to Velopack with Chocolatey support
    Replace the Squirrel.Windows packaging toolchain with Velopack CLI (vpk)
    for creating installers. Update the Chocolatey package scripts to work
    with Velopack's install/uninstall mechanism and modernize the nuspec
    metadata.

--- src/managed/PostBuild.CreateInstaller/createinstaller.cmd
+++ src/managed/PostBuild.CreateInstaller/createinstaller.cmd
@@ -9,23 +9,47 @@ IF "%OLW_CONFIG%" == "" (
   set OLW_CONFIG=Debug
 )
 
-IF EXIST "%LocalAppData%\Nuget\Nuget.exe" (GOTO package) ELSE (
-   echo Nuget.exe missing from %LocalAppData%\Nuget\Nuget.exe
+:: Restore dotnet tools (includes vpk - Velopack CLI)
+dotnet tool restore
+IF %ERRORLEVEL% NEQ 0 (
+   echo Failed to restore dotnet tools. Ensure .config\dotnet-tools.json is present.
    GOTO end
 )
 
-:package
-"%LocalAppData%\Nuget\Nuget.exe" pack .\OpenLiveWriter.nuspec -version %dottedVersion% -basepath src\managed\bin\%OLW_CONFIG%\i386\Writer
-ECHO Created Writer NuGet package.
+:: Create Velopack installer package
+:: --packId: Application identifier (used for install directory name)
+:: --packVersion: Version from version.txt
+:: --packDir: Build output directory containing the application binaries
+:: --mainExe: The main executable to launch
+:: --icon: Application icon for the installer
+:: --outputDir: Where to place the generated installer and update files
+dotnet vpk pack ^
+  --packId OpenLiveWriter ^
+  --packVersion %dottedVersion% ^
+  --packDir src\managed\bin\%OLW_CONFIG%\x64\Writer ^
+  --mainExe OpenLiveWriter.exe ^
+  --icon src\managed\OpenLiveWriter.PostEditor\Images\Writer.ico ^
+  --outputDir Releases
 
-.\src\managed\packages\squirrel.windows.1.4.4\tools\SyncReleases.exe -url=https://olw.blob.core.windows.net/stable/Releases/ -r=.\Releases
-.\src\managed\packages\squirrel.windows.1.4.4\tools\Squirrel.exe -i .\src\managed\OpenLiveWriter.PostEditor\Images\Writer.ico %OLW_SIGN% --no-msi --releasify .\OpenLiveWriter.%dottedVersion%.nupkg 
-MOVE .\Releases\Setup.exe .\Releases\OpenLiveWriterSetup.exe
+IF %ERRORLEVEL% NEQ 0 (
+   echo Velopack packaging failed.
+   GOTO end
+)
+
+MOVE .\Releases\OpenLiveWriter-Setup.exe .\Releases\OpenLiveWriterSetup.exe
+IF %ERRORLEVEL% NEQ 0 (
+   echo Failed to rename OpenLiveWriter-Setup.exe. The file may not have been created by Velopack.
+   GOTO end
+)
 ECHO Created Open Live Writer setup file.
 
-::Build Chocolatey package. Suppress package analysis since Chocolatey powershell generates verbose warnings.
-"%LocalAppData%\Nuget\Nuget.exe" pack .\OpenLiveWriter.Install.nuspec -version %dottedVersion% -basepath Releases -nopackageanalysis
-ECHO Created Writer Chocolatey Package
+:: Build Chocolatey package. Suppress package analysis since Chocolatey powershell generates verbose warnings.
+IF EXIST "%LocalAppData%\Nuget\Nuget.exe" (
+  "%LocalAppData%\Nuget\Nuget.exe" pack .\OpenLiveWriter.Install.nuspec -version %dottedVersion% -basepath Releases -nopackageanalysis
+  ECHO Created Writer Chocolatey Package
+) ELSE (
+  echo Nuget.exe missing from %LocalAppData%\Nuget\Nuget.exe - skipping Chocolatey package
+)
 
 :end
 
